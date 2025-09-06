08cea3c7e33c4b17560306ea4a239475c9cd986e
MD-494 <mansidangat2004@gmail.com>
2025-09-06 21:03:56 +0530

    myfirstcommit

---
+++ Addition/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int a=2;
+        int b=4;
+        int c=a+b;
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Addition is " +c);
+
+    }
+}
---
+++ Student_info/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Mansi Navnath Dangat";
+        int age = 21;
+        double marks = 7.60;
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Name: "+name+"\nAge: "+age+"\nMarks: "+marks);
+    }
+}
